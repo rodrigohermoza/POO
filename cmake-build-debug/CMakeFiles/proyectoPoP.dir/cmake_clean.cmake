ef049755a99aa840dd2ddfcb6b9d79a1b48b0986
Rodrigo Hermoza <rodrigohermoza@MacBook-Pro-de-Admin.local>
2019-10-29 09:21:27 -0500

    primer commit

---
+++ cmake-build-debug/CMakeFiles/proyectoPoP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyectoPoP.dir/main.cpp.o"
+  "proyectoPoP.pdb"
+  "proyectoPoP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyectoPoP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
